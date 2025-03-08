94181f4705d055ae08a02cb8a4c16a05979eb42c
Soran <antien.huang@gmail.com>
2025-03-08 11:54:32 +0000

    217. Contains Duplicate

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,10 @@
+# 217. Contains Duplicate
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        visited = set()
+        for n in nums:
+            if n in visited:
+                return True 
+            visited.add(n)
+        return False
+        
