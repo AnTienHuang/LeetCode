efab461b29fc480c791f24b6c9c7589dbf89447b
Soran <antien.huang@gmail.com>
2025-04-24 09:17:38 +0100

    11. Container With Most Water

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -1,15 +1,32 @@
 # 11. Container With Most Water
 class Solution:
-    def maxArea(self, height: List[int]) -> int: 
-        if len(height) < 2:
-            return 0
+    def maxArea(self, height: list[int]) -> int: 
+    # 2nd attempt
         res = 0
-        l, r = 0, len(height) - 1
+
+        l = 0
+        r = len(height) - 1
+
         while r > l:
-            res = max(res, min(height[l], height[r]) * (r - l))
-            if height[r] > height[l]:
+            area = min(height[l], height[r]) * (r - l)
+            res = max(res, area)
+            if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
         
         return res
+
+    # 1st attempt
+        # if len(height) < 2:
+        #     return 0
+        # res = 0
+        # l, r = 0, len(height) - 1
+        # while r > l:
+        #     res = max(res, min(height[l], height[r]) * (r - l))
+        #     if height[r] > height[l]:
+        #         l += 1
+        #     else:
+        #         r -= 1
+        
+        # return res
